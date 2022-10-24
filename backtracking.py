d69ebf79e2985994ff15e7c79e5846ffc09dc9cd
SKash <112420208+ShivenKashyap03@users.noreply.github.com>
2022-10-24 17:35:29 +0530

    Update backtracking.py

--- backtracking.py
+++ backtracking.py
@@ -1,35 +1,33 @@
-def is_valid(g, r, c, n):
-    for i in range(9):
-        if g[r] == n: return 0
 
-    for j in range(9):
-        if g[c] == n: return 0
-    
-    # same number in 3x3 matrix or not
-    sr = r - r % 3
-    sc = c - c % 3
+N = 9
+def isSafe(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + startCol] == num:
+                return False
+    return True
 
-    for x in range(3):
-        for y in range(3):
-            if g[x + sr][y + sc] == n:
-                return 0
-    return 1
+def Solve(grid, row, col):
+    if (row == N - 1 and col == N):
+        return True
+    if col == N:
+        row += 1
+        col = 0
+    if grid[row][col] != " ":
+        return Solve(grid, row, col + 1)
 
-def Solve(g, r, c):
-    if (r, c) == (8, 9):
-        return 1 # to avoid further backtracking
-
-    if c == 9:
-        r, c = r + 1, 0
-
-    if g[r][c] > 0:
-        return Solve(g, r, c + 1)
-
-    for _ in range(1, 10):
-        if is_valid(g, r, c, _):
-            g[r][c] = _
-            if Solve(g, r, c + 1):
+    for num in range(1, N + 1, 1):
+        if isSafe(grid, row, col, num):
+            grid[row][col] = num
+            if Solve(grid, row, col + 1):
                 return 1
-        g[r][c] = 0
-    return 0
-
+        grid[row][col] = " "
+    return False
